8a1b25baf501389936374431945305d21f6f1ed6
truongshinigami <ntta.thedestroyer@gmail.com>
2020-09-20 07:20:08 +0700

    abc

---
+++ geometric.py
@@ -0,0 +1,10 @@
+x = 10  # biến không cần khai báo
+n = 2.4 # chỉ cần gán giá trị
+x = x * n
+x += 1
+y = x % 3
+s = 'Giá trị của x ='
+b = True
+b = not b
+nullVar = None
+print(s, x, y, b, nullVar)
